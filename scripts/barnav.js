b1d1dd8e33b92ba8a33d994ba933ce9cd5c49cd0
carlos medina <carlosassasin1505@gmail.com>
2023-01-18 20:06:14 -0500

    json v.0.01

--- scripts/barnav.js
+++ scripts/barnav.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
+    $("#barnav").html(`
     
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="https://res.cloudinary.com/donoutoby/image/upload/v1654662652/Cutie_6_j3u3yc.webp" width="35px"></a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../directorio.html">Directorio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Nosostros</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+        </nav>
+    
+    `);
 });
